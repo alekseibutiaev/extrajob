ab2bc637d955681818dffd4d404d4b4e397a66c8
Aleksei Butiaev <alexsyrezerv@mail.ru>
2018-10-01 08:45:16 +0300

    some changes again

--- ogre_tutorial/ogre_tutorial_2.cpp
+++ ogre_tutorial/ogre_tutorial_2.cpp
@@ -3,7 +3,7 @@
 
 int main(int ac, char* av[]) {
   try {
-    std::cout << "Hello world" << std::endl;
+    std::cout << "Hello world!!!" << std::endl;
     return 0;
   }
   catch(const std::exception& e) {
